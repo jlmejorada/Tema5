3bbf04212b22c17736e29cd7681461a13a3a3d47
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-03-05 14:38:46 +0100

    SUBIDA EJ2

---
+++ src/ejercicio02/Libro.java
@@ -0,0 +1,31 @@
+package ejercicio02;
+
+/**
+ * una clase que guarde la información de cada uno de los libros de una
+ * biblioteca.
+ * 
+ * @author jlmejorada
+ */
+public class Libro {
+	
+	/**
+	 *  título del libro
+	 */
+	String titulo="";
+	
+	/**
+	 * autor del libro
+	 */
+	String autor="";
+	
+	/**
+	 * ejemplares totales del libro
+	 */
+	int ejemplares=0;
+	
+	/**
+	 * ejemplares prestados del libro
+	 */
+	int prestados=0;
+	
+}
